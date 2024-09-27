25132c46e1517f6dc76c8183bb666e14b116b959
Reza Kharaei <kharaei.developer@gmail.com>
2024-09-27 20:34:33 +0330

    [feat] Add Cors configuration

--- src/Kharaei.CMS/Program.cs
+++ src/Kharaei.CMS/Program.cs
@@ -2,31 +2,32 @@ using Kharaei.CMS.Models;
 using OrchardCore.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddOrchardCms();
 builder.Host.UseNLogHost();
+
 builder.Services.Configure<SiteSettings>(builder.Configuration.GetSection(nameof(SiteSettings)));
+var origins = builder.Configuration.GetValue<string>("Origins");
+if (string.IsNullOrWhiteSpace(origins))
+    throw new ArgumentNullException("Origin property has not been set!(appsettings issue)");
+
+builder.Services.AddCors(options => 
+{
+    options.AddPolicy(name: "MyCors", x => 
+        x.WithOrigins(origins.Split(','))
+        .AllowCredentials()
+        .AllowAnyMethod()
+        .AllowAnyHeader());
+});
         
-builder.Services
-    .AddOrchardCms()
-    // // Orchard Specific Pipeline
-    // .ConfigureServices( services => {
-    // })
-    // .Configure( (app, routes, services) => {
-    // })
-;
 
 var app = builder.Build();
-
+app.UseCors("MyCors");
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseOrchardCore();
-
 app.Run();
